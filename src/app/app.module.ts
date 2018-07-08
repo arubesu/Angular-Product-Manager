98ed782b9f5129f8c07843d186e80323be7b9579
Bruno Arubesu <bruno.arubesu@gmail.com>
2018-07-08 19:48:32 -0300

    add filter functionality  and some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  isHidden = false;
-  pageTitle = 'Product List';
-  imageWidth = 50;
-  imageMargin = 50;
-  products = [
+export class ProductListComponent implements OnInit {
+  isHidden: boolean = false;
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 10;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[] = [
     {
       'productId': 1,
       'productName': 'Leaf Rake',
@@ -63,7 +77,22 @@ export class ProductListComponent {
     }
   ];
 
+  constructor(){
+    this.filteredProducts = this.products;
+  }
+
+  ngOnInit(): void {
+    this.performFilter('');
+  }
+
+
   hide() {
     this.isHidden = !this.isHidden;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 }
